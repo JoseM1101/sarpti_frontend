63be6f55d1608e9fa1d1981074bd670d7a447545
MiguelC77 <miguelcruzg2002@gmail.com>
2025-03-20 12:32:09 -0400

    investigadores bug fixes.

--- src/components/administration/Investigadores.tsx
+++ src/components/administration/Investigadores.tsx
@@ -74,7 +74,7 @@ const Investigadores: React.FC = () => {
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold">Investigadores</h2>
         <div className="flex space-x-2">
-          {!editingUser && (
+          {!(editingUser || addingUser) && (
             <>
               {isEditing ? (
                 <button
